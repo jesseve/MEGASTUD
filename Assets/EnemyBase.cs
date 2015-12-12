173895a5dc936faef96f67f036cf92a424ff19d2
jesseve <jesseverainen@gmail.com>
2015-12-12 11:00:27 +0200

    update

--- Assets/EnemyBase.cs
+++ Assets/EnemyBase.cs
@@ -1,19 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public abstract class EnemyBase : MonoBehaviour {
+public abstract class EnemyBase : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
 
-public abstract class EnemyUnitBase : IDamageable {
+public abstract class EnemyUnitBase : IDamageable
+{
+    public string name;
+    public int range;
+    public int damage;
+    public int health;
+    public int speed;
 
+    public void TakeDamage(float damage)
+    {
+        Debug.Log(name + " took " + damage + " damage!");
+    }
 }
