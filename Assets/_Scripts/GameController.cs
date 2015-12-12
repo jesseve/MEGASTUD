291e791ded60a4a38f5d00dce85c1642709066b8
jtlinna <jukka.linna@gmail.com>
2015-12-12 17:45:49 +0200

    Game Controller, Resource Buildings

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	[SerializeField] private float money;
+	[SerializeField] private float energy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddResource(ResourceType resource, float amount)
+	{
+		switch(resource)
+		{
+		case ResourceType.Money:
+			money += amount;
+			break;
+
+		case ResourceType.Energy:
+			energy += amount;
+			break;
+		}
+	}
+}
---
+++ Assets/_Scripts/ResourceBuilding.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ResourceType{Money, Energy}
+public class ResourceBuilding : BuildingBase {
+
+	public ResourceType resourceType = ResourceType.Money;
+	public float amountProduced = 250f;
+	public float productionTime = 5f;
+
+	private float timer;
+
+	private GameController gameController;
+
+	void Awake()
+	{
+		gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+		timer = productionTime;
+	}
+
+	override void Update()
+	{
+		base.Update();
+		timer -= Time.deltaTime;
+
+		if(timer <= 0f)
+		{
+			timer = productionTime;
+			gameController.AddResource(resourceType, amountProduced);
+		}
+	}
+}
