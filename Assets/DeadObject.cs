983a9ef5842d9cd07fc4b8281d8cded60d408f20
jtlinna <jukka.linna@gmail.com>
2015-12-13 13:29:40 +0200

    DeadObjectAnim

---
+++ Assets/DeadObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadObject : MonoBehaviour {
+
+	public SpriteRenderer primaryColorSprite;
+	public SpriteRenderer secondaryColorSprite;
+
+}
