50c686fcc374b6364fda473211165f017b6860d0
Joel Joensuu <joeljoensuu@Joel-Joensuu-MacBook-Pro.local>
2015-12-13 15:58:23 +0200

    dazzle not Rocks :D

---
+++ Assets/_Scripts/MenuController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum MenuState {
+	None = -1,
+	MainMenu,
+	Options,
+	Credits
+}
+
+public class MenuController : MonoBehaviour {
+
+	private MenuState currentState = MenuState.None;
+	private Stack<MenuState> states;
+	public GameObject[] menupanels;
+
+	// Use this for initialization
+	void Start () {
+		states = new Stack<MenuState> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeState(int index) {
+		ChangeState ((MenuState)index);
+	}
+
+	public void ChangeState(MenuState newState) {
+		if (newState = currentState) {
+			return;
+		}
+		states.Push (currentState);
+		currentState = newState;
+		HandlePanels ();
+	}
+
+	public void Back() {
+		if (states.Count > 1) {
+			currentState = states.Pop ();
+			HandlePanels ();
+		}
+	}
+
+	private void HandlePanels() {
+		switch (currentState) 
+
+
+	}
+}
