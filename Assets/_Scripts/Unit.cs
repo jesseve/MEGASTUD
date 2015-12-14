aed55458f7278dfd1d094a22a7546803ddfcaf9e
jesseve <jesseverainen@gmail.com>
2015-12-14 20:08:37 +0200

    cx

--- Assets/_Scripts/Unit.cs
+++ Assets/_Scripts/Unit.cs
@@ -150,6 +150,7 @@ public abstract class Unit : MonoBehaviour, IDamageable
     
     protected virtual void EndMove() {
         isMoving = false;
+        SetAnimator("Idle");
 		if(_moveLine != null)
 			_moveLine.enabled = false;
         //SetAnimator("Idle");
